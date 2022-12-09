faf1bb35095079ac98dca3e2abad9328ea6b0e19
jaDelg <alexisjd07@gmail.com>
2022-12-09 16:00:03 -0600

    just adding user and policy claim classes

---
+++ src/main/java/com/revature/PolicyClaim.java
@@ -0,0 +1,52 @@
+package com.revature;
+
+public class PolicyClaim {
+
+	private String description;
+	private String user;
+	private double amount;
+	private String status="pending";
+	//there might be more variables but these work for now. 
+	public PolicyClaim() {
+		
+	}
+	
+	
+	
+	public PolicyClaim(String description, String user, double amount) {
+		super();
+		this.description = description;
+		this.user = user;
+		this.amount = amount;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+
+}
