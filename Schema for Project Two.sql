184711929a7bd6aeb0eb2ea5e8df90870fb9e316
alex-abrams <alexander.abrams99@gmail.com>
2022-12-13 17:27:59 -0800

    Added payments table.

--- Schema for Project Two.sql
+++ Schema for Project Two.sql
@@ -12,9 +12,21 @@ current_subscriber BOOLEAN NOT NULL
 );
 
 CREATE TABLE covid_info(
-covid_id SERIAL PRIMARY KEY,
+covid_id SERIAL PRIMARY KEY, --i think primary keys should just be id to avoid confusion
 user_id INT NOT NULL,
 vaccinated BOOLEAN NOT NULL,
 vaccine_type VARCHAR(100),
 vaccination_date DATE DEFAULT(null)
+);
+
+CREATE TABLE payments(
+id SERIAL PRIMARY KEY NOT NULL CHECK(id > 0),
+user_id INT NOT NULL,
+card_type VARCHAR(20) NOT NULL,
+cvv INT NOT NULL,
+exp_date DATE,
+amount INT NOT NULL,
+-- im not sure if due date is exactly what we want 
+due_date DATE,
+description VARCHAR(100) NOT NULL
 );
