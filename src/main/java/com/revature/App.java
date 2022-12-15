281c2dd63405b687f268c407ff8d039f5bdd587d
jaDelg <alexisjd07@gmail.com>
2022-12-14 21:31:20 -0600

    chagnes to app.java, added controller interface, and updated claim controller

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,9 +1,30 @@
 package com.revature;
 
+
+import com.revature.controllers.Controller;
+
+import io.javalin.Javalin;
+
 public class App {
-	
+private static Javalin app;
 	public static void main(String[] args) {
+		app= Javalin.create();
+		app.get("/hello",(ctx)->{
+			String url = ctx.url();
+			System.out.println(url);
+			ctx.html("<h1> Hello Javalin</h1>");
+			ctx.status(200);
+		} );
+		
+		configure();//controllers go in here
+		
+		app.start();
 		
 	}
 
+	public static void configure(Controller... controllers) {
+		for(Controller c:controllers) {
+			c.addRoutes(app);
+		}
+	}
 }
---
+++ src/main/java/com/revature/controllers/ClaimController.java
@@ -0,0 +1,19 @@
+package com.revature.controllers;
+
+import io.javalin.Javalin;
+
+public class ClaimController implements Controller{
+
+	public ClaimController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void addRoutes(Javalin app) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
