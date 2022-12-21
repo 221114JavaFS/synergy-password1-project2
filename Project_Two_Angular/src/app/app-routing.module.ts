2fd725b20f0b691a1f97733d3c7d717ed2db5f25
jaDelg <alexisjd07@gmail.com>
2022-12-21 12:39:40 -0600

    added a main menu

--- Project_Two_Angular/src/app/app-routing.module.ts
+++ Project_Two_Angular/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClaimDetailComponent } from './claim-detail/claim-detail.component';
 import { ClaimsComponent } from './claims/claims.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/main-menu', pathMatch: 'full' },
   { path: 'detail/:claim_id', component: ClaimDetailComponent },
-  { path: 'claims', component: ClaimsComponent }
+  { path: 'claims', component: ClaimsComponent },
+  {path: 'main-menu', component: MainMenuComponent }
 ];
 
 @NgModule({
--- Project_Two_Angular/src/app/app.module.ts
+++ Project_Two_Angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClaimsComponent } from './claims/claims.component';
 import { ClaimDetailComponent } from './claim-detail/claim-detail.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClaimsComponent,
-    ClaimDetailComponent
+    ClaimDetailComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
