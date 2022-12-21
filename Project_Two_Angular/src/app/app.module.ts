e54efb620751d5382b3ce84f560822f95d9f18ee
jaDelg <alexisjd07@gmail.com>
2022-12-21 10:52:47 -0600

    erased app.comp.html

--- Project_Two_Angular/src/app/app.module.ts
+++ Project_Two_Angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClaimsComponent } from './claims/claims.component';
+import { ClaimDetailComponent } from './claim-detail/claim-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClaimsComponent,
+    ClaimDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ Project_Two_Angular/src/app/claim-detail/claim-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-detail',
+  templateUrl: './claim-detail.component.html',
+  styleUrls: ['./claim-detail.component.css']
+})
+export class ClaimDetailComponent {
+
+}
